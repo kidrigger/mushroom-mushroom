b8dac9b273fdb607c110c4d3b9815d08dec1bee9
czinczar <mrad@gmx.com>
2023-11-21 08:29:30 +0100

    added bouncing mushroom

--- bounce_mushroom.gd
+++ bounce_mushroom.gd
@@ -4,6 +4,15 @@ extends Node3D
 var boost_speed: float
 var shroom_normal: Vector3
 
+@onready var mushroom_model = $"mushroom-1"
+@onready var pop_audio_stream_player_3d = $"pop-AudioStreamPlayer3D"
+@onready var boing_audio_stream_player_3d_2 = $"boing-AudioStreamPlayer3D2"
+
+func _ready():
+	print(mushroom_model)
+	mushroom_model.get_node("AnimationPlayer").play("spawn", -1, 4, false)
+	pop_audio_stream_player_3d.play()
+
 func initialize(position: Vector3, surface_normal: Vector3, player_position: Vector3):
 	var normal = surface_normal.normalized()
 	var player_dir = player_position - position
@@ -12,6 +21,7 @@ func initialize(position: Vector3, surface_normal: Vector3, player_position: Vec
 	shroom_normal = normal
 	
 	look_at_from_position(position, position + forward, normal)
+	
 
 var possible_body_found = false
 var possible_body_velocity: Vector3
@@ -22,6 +32,9 @@ func _on_body_entered(body: Node):
 		if possible_body_found:
 			body.velocity = possible_body_velocity - shroom_normal * shroom_normal.dot(possible_body_velocity) + boost_speed * shroom_normal
 			possible_body_found = false
+			boing_audio_stream_player_3d_2.set_pitch_scale(randf_range(0.85, 1.25))
+
+			boing_audio_stream_player_3d_2.play()
 		else:
 			body.velocity = body.velocity - 2 * shroom_normal * shroom_normal.dot(body.velocity)
 
